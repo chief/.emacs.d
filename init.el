616936c43f0b2564d8a369e6a8906aac334f20de
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-14 12:02:09 +0300

    packages: Add tramp

--- init.el
+++ init.el
@@ -506,32 +506,6 @@ comint-replace-by-expanded-history-before-point."
 (use-package dockerfile-mode
   :mode (("Dockerfile\\'" . dockerfile-mode)))
 
-;; Tramp settings
-(use-package tramp
-  :defer 5
-  :config
-  ;; Turn of auto-save for tramp files
-  (defun tramp-set-auto-save ()
-    (auto-save-mode -1))
-  (with-eval-after-load 'tramp-cache
-    (setq tramp-persistency-file-name "~/.emacs.d/etc/tramp"))
-  (setq tramp-default-method "ssh"
-        tramp-default-user-alist '(("\\`su\\(do\\)?\\'" nil "root"))
-        tramp-adb-program "adb"
-        ;; use the settings in ~/.ssh/config instead of Tramp's
-        tramp-use-ssh-controlmaster-options nil
-        backup-enable-predicate
-        (lambda (name)
-          (and (normal-backup-enable-predicate name)
-               (not (let ((method (file-remote-p name 'method)))
-                      (when (stringp method)
-                        (member method '("su" "sudo"))))))))
-
-  (use-package tramp-sh
-    :config
-    (add-to-list 'tramp-remote-path "/usr/local/sbin")
-    (add-to-list 'tramp-remote-path "/opt/java/current/bin")
-    (add-to-list 'tramp-remote-path "~/bin")))
 
 ;; Spell check and flyspell settings
 ;; ---------------------------------
---
+++ packages/tramp.el
@@ -0,0 +1,29 @@
+;;; tramp --- (Transparent Remote Access, Multiple Protocols) is a package for
+;;; editing remote files, similar to AngeFtp or efs
+;;; Commentary: https://www.emacswiki.org/emacs/TrampMode
+(use-package tramp
+  :ensure t
+  :defer 5
+  :config
+  ;; Turn off auto-save for tramp files
+  (defun tramp-set-auto-save ()
+    (auto-save-mode nil))
+  (with-eval-after-load 'tramp-cache
+    (setq tramp-persistency-file-name "~/.emacs.d/etc/tramp"))
+  (setq tramp-default-method "ssh"
+        tramp-default-user-alist '(("\\`su\\(do\\)?\\'" nil "root"))
+        tramp-adb-program "adb"
+        ;; use the settings in ~/.ssh/config instead of Tramp's
+        tramp-use-ssh-controlmaster-options nil
+        backup-enable-predicate
+        (lambda (name)
+          (and (normal-backup-enable-predicate name)
+               (not (let ((method (file-remote-p name 'method)))
+                      (when (stringp method)
+                        (member method '("su" "sudo"))))))))
+
+  (use-package tramp-sh
+    :config
+    (add-to-list 'tramp-remote-path "/usr/local/sbin")
+    (add-to-list 'tramp-remote-path "/opt/java/current/bin")
+    (add-to-list 'tramp-remote-path "~/bin")))
