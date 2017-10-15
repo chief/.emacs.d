89fae73905523974d38cfb81912b2431b9de32d0
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-15 21:16:57 +0300

    Delete files

--- packages/windmove.el
+++
@@ -1,7 +0,0 @@
-;;; windmove.el --- directional window-selection routines
-;;; https://github.com/emacs-mirror/emacs/blob/master/lisp/windmove.el
-(use-package windmove
-  :init
-  (setq windmove-wrap-around t)
-  :config
-  (windmove-default-keybindings))
