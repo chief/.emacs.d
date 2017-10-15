a657b5a4cdfee90055db3ceeb708940ab894a2aa
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-15 21:43:30 +0300

    Remove saveplace, uniquify packages

--- packages/saveplace.el
+++
@@ -1,7 +0,0 @@
-;;; saveplace.el --- automatically save place in files
-;;; https://github.com/emacs-mirror/emacs/blob/master/lisp/saveplace.el
-(use-package saveplace
-  :defer t
-  :init
-  (setq-default save-place t)
-  (setq save-place-file (expand-file-name ".places" user-emacs-directory)))
--- packages/uniquify.el
+++
@@ -1,5 +0,0 @@
-;;; uniquify ---  Unique buffer names dependent on file name
-;;; Commentary: https://github.com/emacs-mirror/emacs/blob/master/lisp/uniquify.el
-(use-package uniquify
-  :init
-  (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
