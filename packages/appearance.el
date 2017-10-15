b4b747818c5539076700f3a8fbed3ddcbdbbaa41
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-15 21:08:05 +0300

    packages: Move diminish to appearance

--- packages/appearance.el
+++ packages/appearance.el
@@ -10,3 +10,11 @@
   :config
   (setq sml/shorten-directory t
         sml/shorten-modes t))
+
+;;; diminish --- Diminished modes are minor modes with no modeline display
+;;; Commentary: https://github.com/emacsmirror/diminish
+(use-package diminish
+  :ensure t
+  :init
+  (progn
+    (diminish 'auto-fill-function "")))
--- packages/diminish.el
+++
@@ -1,7 +0,0 @@
-;;; diminish --- Diminished modes are minor modes with no modeline display
-;;; Commentary: https://github.com/emacsmirror/diminish
-(use-package diminish
-  :ensure t
-  :init
-  (progn
-    (diminish 'auto-fill-function "")))
