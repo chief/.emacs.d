72e246f8fa0147f3cbf9e825767bc2c23ecb9084
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-18 23:11:01 +0300

    packages: Move beacon and rainbow-mode to visual

--- init.el
+++ init.el
@@ -111,17 +111,6 @@
 (use-package better-defaults
   :ensure t)
 
-;;; rainbow-mode  colorize string with color information
-;;; https://github.com/emacsmirror/rainbow-mode/blob/master/rainbow-mode.el
-(use-package rainbow-mode
-  :ensure t
-  :commands rainbow-mode)
-
-;;; beacon --- A light that follows your cursor around so you don't lose it!
-;;; https://github.com/Malabarba/beacon
-(use-package beacon
-  :ensure t
-  :commands beacon-mode)
 
 ;;; popup --- Visual Popup Interface Library for Emacs
 ;;; https://github.com/auto-complete/popup-el
@@ -1032,13 +1021,7 @@ comint-replace-by-expanded-history-before-point."
   :disabled t
   :bind (("C-c i" . imenu-anywhere)))
 
-;; beacon
-;; ------
 
-;; Flash cursor whenever you adjust position.
-(use-package beacon
-  :diminish beacon-mode
-  :init (beacon-mode 1))
 
 ;; smartscan
 ;; ---------
---
+++ packages/visual.el
@@ -0,0 +1,11 @@
+;;; rainbow-mode  colorize string with color information
+;;; https://github.com/emacsmirror/rainbow-mode/blob/master/rainbow-mode.el
+(use-package rainbow-mode
+  :ensure t
+  :commands rainbow-mode)
+
+;;; beacon --- A light that follows your cursor around so you don't lose it!
+;;; https://github.com/Malabarba/beacon
+(use-package beacon
+  :ensure t
+  :commands beacon-mode)
