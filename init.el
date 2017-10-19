4a73031eea7ae2189df7287b5c33334725d0f2ce
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-10-19 23:49:24 +0300

    Ensure org and org-bullets

--- init.el
+++ init.el
@@ -468,12 +468,14 @@ comint-replace-by-expanded-history-before-point."
 ;; Org-mode
 ;; --------
 (use-package org
+  :ensure t
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c b" . org-iswitchb)))
 
 ;; org-bullets
 (use-package org-bullets
+  :ensure t
   :init
   (add-hook 'org-mode-hook #'org-bullets-mode))
 
