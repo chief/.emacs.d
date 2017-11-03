48e7e42a2f330e6b71fc5937383f3ec73587df0f
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2017-11-03 22:12:09 +0200

    Add packaging packages

---
+++ packages/packaging.el
@@ -0,0 +1,6 @@
+;;; paradox --- Project for modernizing Emacs' Package Menu. With package ratings, usage statistics, customizability, and more.
+;;; https://github.com/Malabarba/paradox
+(use-package paradox
+  :ensure t
+  :config
+  (paradox-enable))
