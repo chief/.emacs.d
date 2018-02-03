4a15a4ed8fdb22782ef8166ed84c1170b4240a1b
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2018-02-03 16:42:23 +0200

    Set backup tmp dir

--- init.el
+++ init.el
@@ -100,6 +100,15 @@
 
 (setq auto-save-default t)
 
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
 ;;
 ;; Editor settings
 ;;
