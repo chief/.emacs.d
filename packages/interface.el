fb3fcf5aa6000e32b09e14b69eeec73084dddfa4
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2018-10-31 10:17:00 +0200

    Change keybind for helm-projectile-find

--- packages/interface.el
+++ packages/interface.el
@@ -72,7 +72,7 @@
 
   (global-set-key (kbd "C-c h") 'helm-command-prefix)
   (global-set-key (kbd "s-p") 'helm-projectile-find-file)
-  (global-set-key (kbd "s-;") 'helm-projectile-ag)
+  (global-set-key (kbd "s-[") 'helm-projectile-ag)
   (global-unset-key (kbd "C-x c"))
 
   (setq
