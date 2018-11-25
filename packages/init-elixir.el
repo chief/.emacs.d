b983e370b05028a440c9df3675fea0f66436eb4d
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2018-11-25 20:17:43 +0200

    Add basic support for Elixir programming language

---
+++ packages/init-elixir.el
@@ -0,0 +1,10 @@
+;;; elixir-mode ---- Emacs major mode for Elixir
+;;; https://github.com/elixir-editors/emacs-elixir
+(use-package elixir-mode
+  :ensure t)
+
+
+;;; alchemist --- Elixir Tooling Integration Into Emacs
+;;; https://github.com/tonini/alchemist.el
+(use-package alchemist
+  :ensure t)
